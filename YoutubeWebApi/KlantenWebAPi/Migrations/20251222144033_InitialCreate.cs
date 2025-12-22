d641f7d02a4852b645e868bfab0b8ee929d953f2
soulinee <91409289+soulinee@users.noreply.github.com>
2025-12-22 18:05:08 +0100

    initial migration gemaakt

---
+++ YoutubeWebApi/KlantenWebAPi/Migrations/20251222144033_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KlantenWebAPi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Klanten",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VolledigeNaam = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Emailadres = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    Wachtwoord = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Klanten", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Klanten");
+        }
+    }
+}
